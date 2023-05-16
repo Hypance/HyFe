ccdc3260e5845016eb9d922c15e77bd04ca14f6d
Berkan <berkan1458@gmail.com>
2023-05-16 20:46:49 +0300

    prettier format

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,54 +1,34 @@
 import { createGlobalStyle } from 'styled-components'
-
 export const GlobalStyle = createGlobalStyle`
-
-
 
 :root{
     --title-color: #062046;
     --comment-color: #757575;
-
-    
 }
-
 p{
     margin: 0;
     padding: 0;
 }
-
-
-
 .page-title{
     color:var(--title-color);
     font-weight:600;
 }
-
 .card-title{
     color: var(--title-color); 
     font-weight:600;
 }
-
 .card-comment{
     color: var(--comment-color);
     font-weight:500;
 }
-
 .transactions-card{
     overflow-y:scroll;
     max-height:325px;
 }
 
-
 .strategy-signals-height{
     min-height:140px;   
-    
-    
 }
-
-
 .strategy-card-buttons{
 }
-
-
-
 `
