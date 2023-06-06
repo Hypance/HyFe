7151f2ef7a836958d4f9a89dd8fcd8ccaa0ed8d3
Berkan <berkan1458@gmail.com>
2023-06-06 10:54:31 +0300

    created BotDetailsPage

---
+++ src/pages/Bots/BotDetails.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+export const BotDetails:React.FC = () => {
+
+  let { botId } = useParams();
+
+  // botId ile API 'a istek atılacak bot details çekilecek.
+
+  // useEffect(() => {
+  //   const fetchBot = async () => {
+  //     const data = await botServiceGetBot(botId);
+  //     setBot(data);
+  //   };
+  //   fetchBot();
+  // }, [botId]);
+
+  return (
+    <div>BotDetails {botId}</div>
+
+  )
+}
