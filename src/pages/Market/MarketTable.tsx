7d05e726b2a7f24c488dc9f40611453b98121769
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2023-08-22 21:18:27 +0300

    wip:: marketTable

--- src/pages/Market/MarketTable.tsx
+++ src/pages/Market/MarketTable.tsx
@@ -1,24 +1,25 @@
 import React, { useEffect, useState } from "react";
 import { Table } from "react-bootstrap";
 
-interface MarketData {
-  symbol: string;
-  priceChangePercent: number;
-  volume: number;
-  closePrice: string;
+interface KlineData {
+  t: number;
+  o: string;
+  c: string;
+  h: string;
+  l: string;
 }
 
 const MarketTable = () => {
-  const [marketData, setMarketData] = useState<{ [symbol: string]: MarketData }>({});
+  const [klineData, setKlineData] = useState<KlineData[]>([]);
 
   useEffect(() => {
-    const socket = new WebSocket("wss://stream.binance.com:9443/ws");
+    const socket = new WebSocket("wss://stream.binance.com:9443/ws/btcusdt@kline_1h/btcusdt@kline_4h/btcusdt@kline_1d");
 
     socket.onopen = () => {
       socket.send(
         JSON.stringify({
           method: "SUBSCRIBE",
-          params: ["btcusdt@ticker", "bnbusdt@ticker", "xrpusdt@ticker"],
+          params: ["btcusdt@kline_1h","btcusdt@kline_4h","btcusdt@kline_1d"],
           id: 1,
         })
       );
@@ -26,17 +27,10 @@ const MarketTable = () => {
 
     socket.onmessage = (event) => {
       const data = JSON.parse(event.data);
-
-      if (data.e === "24hrTicker") {
-        setMarketData((prevData) => ({
-          ...prevData,
-          [data.s]: {
-            symbol: data.s,
-            priceChangePercent: data.P,
-            volume: data.v,
-            closePrice: data.c
-          },
-        }));
+      
+      if (data.e === "kline") {
+        const kline = data.k;
+        setKlineData((prevData) => [...prevData, kline]);
       }
     };
 
@@ -45,29 +39,44 @@ const MarketTable = () => {
     };
   }, []);
 
-  return (
-    <div>
-      <h1>Table</h1>
-      <Table>
-        <thead>
-          <tr>
-            <th>Symbol</th>
-            <th>Change</th>
-            <th>Volume</th>
-            <th>Close Price</th>
-          </tr>
-        </thead>
-        <tbody>
-          {Object.values(marketData).map((data, index) => (
-            <tr key={index}>
-              <td>{data.symbol}</td>
-              <td>{data.priceChangePercent}</td>
-              <td>{data.volume}</td>
-              <td>{data.closePrice}</td>
+  const renderKlineColumn = (data: KlineData[], title: string) => {
+    return (
+      <div className="column">
+        <h2>{title}</h2>
+        <Table>
+          <thead>
+            <tr>
+              <th>Timestamp</th>
+              <th>Open</th>
+              <th>Close</th>
+              <th>High</th>
+              <th>Low</th>
             </tr>
-          ))}
-        </tbody>
-      </Table>
+          </thead>
+          <tbody>
+            {data.map((kline, index) => (
+              <tr key={index}>
+                <td>{new Date(kline.t).toLocaleString()}</td>
+                <td>{kline.o}</td>
+                <td>{kline.c}</td>
+                <td>{kline.h}</td>
+                <td>{kline.l}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+    );
+  };
+
+  return (
+    <div className="container">
+      <h1>Kline Data Table</h1>
+      <div className="columns">
+        {renderKlineColumn(klineData, "1 Hour (1h)")}
+        {renderKlineColumn(klineData, "4 Hours (4h)")}
+        {renderKlineColumn(klineData, "1 Day (1d)")}
+      </div>
     </div>
   );
 };
