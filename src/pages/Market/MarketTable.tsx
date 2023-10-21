bbe1564f64713a7aea67f766b7692895ebfcee71
Berk Tenkoğlu <berk.tenkoglu@gmail.com>
2023-10-21 11:40:29 +0300

    updated market table

--- src/pages/Market/MarketTable.tsx
+++ src/pages/Market/MarketTable.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { Table } from "react-bootstrap";
+import { Col,  Form } from "react-bootstrap";
 import { useFetchCoins } from "../../hooks/useFetchCoins";
+import DataTable, { TableColumn } from "react-data-table-component";
 
 interface KlineData {
   s: string;
@@ -13,14 +14,22 @@ interface KlineData {
 
 const MarketTable = () => {
   const [klineData, setKlineData] = useState<KlineData[]>([]);
+  const [klineInterval, setKlineInterval] = useState("1m");
   const coins = useFetchCoins();
-  let coinKlines = coins.map((c)=>{return `${c.name.toLowerCase()}@kline_1m`})
-  // console.log(coinKlines);
+  let coinKlines = coins.map((c)=>{return `${c.name.toLowerCase()}@kline_${klineInterval}`})
   
+  
+  // console.log(coinKlines);
+  const onChangeInterval = (e:any) => {
+    setKlineInterval(e.target.value)
+    
+
+  }
   useEffect(() => {
     const socket = new WebSocket("wss://stream.binance.com:9443/ws");
-
     socket.onopen = () => {
+      console.log(coinKlines);
+      
       socket.send(
         JSON.stringify({
           method: "SUBSCRIBE",
@@ -37,88 +46,163 @@ const MarketTable = () => {
     socket.onmessage = (event) => {
       const data = JSON.parse(event.data);
       // console.log(data.e);
-      console.log(klineData);
+      // console.log(klineData);
       if (data.e === "kline") {
         const kline = data.k;
+        console.log(kline);
+        
   
-        let kData = klineData.find(k=>k.s.toLowerCase()==kline.s.toLowerCase())
-        
-        
-        
-        if(kData !== undefined) {
-          console.log(kData.s + " "+kline.s);
-          console.log("deneme");
+        setKlineData((prevData) => {
+          const existingIndex = prevData.findIndex(
+            (item) => item.s.toLowerCase() === kline.s.toLowerCase()
+          );
           
-          kData.o = kline.o;
-          kData.c = kline.c;
-          kData.h = kline.h;
-          kData.l = kline.l;
-          setKlineData((prevData)=> prevData.filter(e =>{
-            if(e.s !==data.k.s){
-              console.log(e);
-              
-              return e;
-            } else if(e.s == data.k.s){
-    
-              e.o=data.k.o;
-              console.log(e);
-              
-              return e;
-            } else{
-              return e;
-            }
-          }))
-        } else{
-          setKlineData((prevData) => [...prevData, kline]);
-        }
-       
+          
+
+          if (existingIndex !== -1) {
+            // İsim aynı ise veriyi güncelle
+            prevData[existingIndex] = kline;
+            return [...prevData];
+          } else {
+            // İsim aynı değilse yeni veriyi ekle
+            return [...prevData, kline];
+          }
+        
+        });
       }
     };
-
-    return () => {
-      socket.close();
-    };
-  }, [coins]);
-
+  return () => {
+    socket.close()
+  }
+  }, [coins,klineInterval]);
+  function calculatePriceDif(openPrice:number, closePrice:number) {
+    // Fark yüzdesini hesapla
+    const priceDifferencePercentage = ((closePrice - openPrice) / openPrice) * 100;
+    return priceDifferencePercentage;
+  }
+  
   
 
-  const renderKlineColumn = (data: KlineData[], title: string) => {
+  const columns: TableColumn<KlineData>[] = [
+    {
+        name: 'Symbol',
+        selector: row => row.s,
+        sortable: true,
+    },
+    {
+        name: 'Timestamp',
+        selector: row => new Date(row.t).toLocaleString(),
+        sortable: true,
+    },
+    {
+        name: 'Open',
+        selector: row => row.o,
+        sortable: true,
+    },
+    {
+      name: 'Close',
+      selector: row => row.c,
+      sortable: true,
+  },
+  {
+    name: 'High',
+    selector: row => row.h,
+    sortable: true,
+},
+{
+  name: 'Low',
+  selector: row => row.l,
+  sortable: true,
+},
+{
+  name: "Change %",
+  selector: row => calculatePriceDif(Number(row.o), Number(row.c)).toFixed(2),
+  sortable:true,
+  conditionalCellStyles:
+  [{
+    when: row => calculatePriceDif(Number(row.o), Number(row.c)) < 0,
+    style: {
+      
+      color: 'red',
+      
+    },
+  },
+  {
+    when: row => calculatePriceDif(Number(row.o), Number(row.c)) > 0,
+    style: {
+      
+      color: 'green',
+      
+    },
+  }
+]
+}
+];
+
+
+  const renderKlineColumn = (data: KlineData[]) => {
     return (
-      <div className="column">
-        <h2>{title}</h2>
-        <Table>
-          <thead>
-            <tr>
-              <th>Symbol</th>
-              <th>Timestamp</th>
-              <th>Open</th>
-              <th>Close</th>
-              <th>High</th>
-              <th>Low</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((kline, index) => (
-              <tr key={index}>
-                <td>{kline.s}</td>
-                <td>{new Date(kline.t).toLocaleString()}</td>
-                <td>{kline.o}</td>
-                <td>{kline.c}</td>
-                <td>{kline.h}</td>
-                <td>{kline.l}</td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-      </div>
+      <DataTable columns={columns} data={data} />
+
+      // <div className="column">
+        
+      //   <Table>
+      //     <thead>
+      //       <tr>
+      //         <th>Symbol</th>
+      //         <th>Timestamp</th>
+      //         <th>Open</th>
+      //         <th>Close</th>
+      //         <th>High</th>
+      //         <th>Low</th>
+      //       </tr>
+      //     </thead>
+      //     <tbody>
+      //       {data.map((kline, index) => (
+      //         <tr key={index}>
+      //           <td>{kline.s}</td>
+      //           <td>{new Date(kline.t).toLocaleString()}</td>
+      //           <td>{kline.o}</td>
+      //           <td>{kline.c}</td>
+      //           <td>{kline.h}</td>
+      //           <td>{kline.l}</td>
+      //         </tr>
+      //       ))}
+      //     </tbody>
+      //   </Table>
+      // </div>
     );
   };
 
   return (
     <div className="container">
-      <h1>Kline Data Table</h1>
+      <div>
+      <Col xs sm="6">
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Predefined Market</Form.Label>
+              <Form.Select
+                onChange={onChangeInterval}
+              >
+                 
+                  <option key="1" value="1m">
+                    1 minute
+                  </option>
+                  <option key="2" value="1h">
+                    1 hour
+                  </option>
+                  <option key="3" value="4h">
+                    4 hours
+                  </option>
+                  <option key="4" value="1d">
+                    1 day
+                  </option>
+                
+              </Form.Select>
+            </Form.Group>
+          </Col>
+      </div>
       <div className="columns">
-        {renderKlineColumn(klineData, "1 Minute (1m)")}
+        {renderKlineColumn(klineData)}
 
       </div>
     </div>
